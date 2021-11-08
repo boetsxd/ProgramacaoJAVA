bc9b9a26e7860a416407a1158ab55f56087bc073
Alex Silva <48893146+boetsxd@users.noreply.github.com>
2021-11-08 20:55:43 -0300

    Adição de Desafio Inter

---
+++ DesafioLaranja/src/digitoUnico/SomaDigito.java
@@ -0,0 +1,17 @@
+package digitoUnico;
+
+/*@author lexsilvadj*/
+
+import java.util.Scanner;
+
+public class SomaDigito {
+    public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);    
+       String n;
+       int k;
+       int p;
+System.out.println("Insira o número a ser concatenado" );
+n = sc.nextLine();
+    }   
+    sc.close();   
+}
